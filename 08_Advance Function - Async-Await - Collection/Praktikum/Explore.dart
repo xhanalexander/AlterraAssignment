d731e85887c39674e8baa099b10e38229eb27f2b
xhanalexander <xhanalexander@gmail.com>
2023-02-24 02:09:39 +0700

    minor fixed

--- 08_Advance Function - Async-Await - Collection/Praktikum/Explore.dart
+++ 08_Advance Function - Async-Await - Collection/Praktikum/Explore.dart
@@ -1,5 +1,47 @@
 // import 'dart:io';
 
 void main() {
-  
+  carList();
+  redundant();
+}
+
+void carList() {
+  List<String> cars = [
+    'Amuse',
+    'Tommy Kaira',
+    'Spoon',
+    'HKS',
+    'Litchfield',
+    'Amuse',
+    'HKS'
+  ];
+  List<String> cars2 = [
+    'JS Racing',
+    'amuse',
+    'spoon',
+    'spoon',
+    'JS Racing',
+    'amuse'
+  ];
+
+  var carTrim = cars.toSet();
+  var carTrim2 = cars2.toSet();
+
+  print(carTrim);
+  print(carTrim2);
+}
+
+void redundant() {
+  List<int> numbers = [1, 2, 2, 3, 4, 4, 4, 5, 5];
+
+  var countMap = <int, int>{};
+  for (var num in numbers) {
+    countMap[num] = (countMap[num] ?? 0) + 1;
+  }
+
+  var duplicates = countMap.entries.where((entry) => entry.value > 1);
+  print('Number of duplicates: ${duplicates.length}');
+  for (var entry in duplicates) {
+    print('Number ${entry.key} has ${entry.value} occurrences');
+  }
 }
