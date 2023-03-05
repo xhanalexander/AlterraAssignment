571d34fa11fcc1b4249c8867a75e154a3de75173
xhanalexander <xhanalexander@gmail.com>
2023-03-06 02:28:47 +0700

    minor adds

---
+++ 11_Introduction Flutter Widget/betatest/lib/homePage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePages extends StatelessWidget {
+  const HomePages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: 400,
+        height: 200,
+        color: Colors.red,
+      ),
+    );
+  }
+}
--- 11_Introduction Flutter Widget/betatest/lib/main.dart
+++ 11_Introduction Flutter Widget/betatest/lib/main.dart
@@ -1,12 +1,15 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:html';
+
+import 'package:betatest/homePage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 /* 
-  class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
@@ -14,31 +17,48 @@ void main() {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('NewApp', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+          title: const Text('NewApp',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black)),
           backgroundColor: Colors.transparent,
           shadowColor: Colors.transparent,
         ),
         body: Center(
-          child: Column (
+          child: Column(
             children: [
               Container(
                 margin: const EdgeInsets.all(10),
                 width: 400,
                 height: 200,
                 color: Colors.red,
-                child: const Center(child: Text('Container 1', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black))),
+                child: const Center(
+                    child: Text('Container 1',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black))),
               ),
               Container(
                 width: 200,
                 height: 200,
                 color: Colors.green,
-                child: const Text('Container 2', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+                child: const Text('Container 2',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black)),
               ),
               Container(
                 width: 200,
                 height: 200,
                 color: Colors.blue,
-                child: const Text('Container 3', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+                child: const Text('Container 3',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black)),
               ),
             ],
           ),
@@ -47,46 +67,26 @@ void main() {
     );
   }
 } 
-*/
+ */
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
+    return const CupertinoApp(
+      debugShowCheckedModeBanner: true,
       home: CupertinoPageScaffold(
-        navigationBar: const CupertinoNavigationBar(
-          middle: Text('NewApp', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
-          backgroundColor: Colors.transparent,
+        navigationBar: CupertinoNavigationBar(
+          middle: Text('Headlines'),
         ),
-        child: Center(
-          child: Column (
-            children: [
-              Container(
-                margin: const EdgeInsets.all(10),
-                width: 400,
-                height: 200,
-                color: Colors.red,
-                child: const Center(child: Text('Container 1', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black))),
-              ),
-              Container(
-                width: 200,
-                height: 200,
-                color: Colors.green,
-                child: const Text('Container 2', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
-              ),
-              Container(
-                width: 200,
-                height: 200,
-                color: Colors.blue,
-                child: const Text('Container 3', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
-              ),
-            ],
+        // child: HomePages(),
+        child: Scaffold(
+          body: Center(
+            child: Text('Hello World'),
           ),
-        ),
-      ),
+        )
+      )
     );
   }
 }
-
