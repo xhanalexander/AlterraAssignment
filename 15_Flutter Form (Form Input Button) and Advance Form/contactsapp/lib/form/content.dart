5ee0af6a1fb84ffbc31d1dccf59b0ede0d454311
xhanalexander <xhanalexander@gmail.com>
2023-03-21 01:40:38 +0700

    added dynamic contact

--- 15_Flutter Form (Form Input Button) and Advance Form/contactsapp/lib/form/content.dart
+++ 15_Flutter Form (Form Input Button) and Advance Form/contactsapp/lib/form/content.dart
@@ -1,5 +1,82 @@
 import 'package:flutter/material.dart';
 
+final List<Map<String, String>> contactList = [
+  {"avatar": "L", "name": "Leanne Graham", "phone": "1-770-736-8031 x56442"},
+  {"avatar": "E", "name": "Ervin Howell", "phone": "010-692-6593 x09125"},
+  {"avatar": "C", "name": "Clementine Bauch", "phone": "1-463-123-4447"},
+  {"avatar": "S", "name": "Patricia Lebsack", "phone": "493-170-9623 x156"},
+  {"avatar": "C", "name": "Chelsey Dietrich", "phone": "(254)954-1289"},
+  {"avatar": "K", "name": "Kurtis Weissnat", "phone": "210.067.6132"},
+  {"avatar": "Z", "name": "Zenaya Krakozia", "phone": "434-170-9623 x156"},
+  {"avatar": "D", "name": "Dimitri Rascalov", "phone": "210.067.6132"},
+];
+
+class allContact extends StatefulWidget {
+  const allContact({super.key});
+
+  @override
+  State<allContact> createState() => _allContactState();
+}
+
+class _allContactState extends State<allContact> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: const Color(0xff6750A4),
+          title: const Text('Contacts List', style: TextStyle(color: Colors.white)),
+          centerTitle: true,
+        ),
+      body: ListView.builder(
+        itemCount: contactList.length,
+        itemBuilder: (BuildContext context, int i) {
+          final contact = contactList[i];
+          if (contactList != null) {
+            return ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Color(0xff6750A4),
+              radius: 20,
+              child: Text(
+                contact["avatar"]!,
+                style: const TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+            trailing: IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                setState(() {
+                  contactList.removeAt(i);
+                });
+              },
+            ),
+            title: Text(contact["name"]!),
+            subtitle: Text(contact["phone"]!),
+            ); 
+          } else {
+            return const Center(
+              child: Text("No Contact Found", style: TextStyle(fontSize: 20),),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color(0xff6750A4),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const ContactForms()),
+          );
+        },
+        child: const Icon(Icons.add),
+      )
+    );
+  }
+}
+
+// 
+
 class ContactForms extends StatefulWidget {
   const ContactForms({super.key});
 
@@ -10,6 +87,7 @@ class ContactForms extends StatefulWidget {
 class _ContactFormsState extends State<ContactForms> {
   var nameKey = GlobalKey<FormState>();
   var phoneKey = GlobalKey<FormState>();
+  var phoneController = TextEditingController();
   var nameController = TextEditingController();
 
   @override
@@ -53,12 +131,6 @@ class _ContactFormsState extends State<ContactForms> {
                   child: Form(
                     key: nameKey,
                     child: TextFormField(
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return "Please insert your name";
-                        }
-                        return null;
-                      },
                       controller: nameController,
                       keyboardType: TextInputType.number,
                       decoration: InputDecoration(
@@ -77,7 +149,14 @@ class _ContactFormsState extends State<ContactForms> {
                           Icons.person,
                           color: Color(0xff6750A4),
                         ),
+                        
                       ),
+                      validator: (value) {
+                        if (value!.isEmpty || value.length < 3) {
+                          return "Please insert your name";
+                        }
+                        return null;
+                      },
                     ),
                   )),
               const SizedBox(height: 20,),
@@ -86,6 +165,7 @@ class _ContactFormsState extends State<ContactForms> {
                   child: Form(
                       key: phoneKey,
                       child: TextFormField(
+                        controller: phoneController,
                         keyboardType: TextInputType.number,
                         decoration: InputDecoration(
                           enabledBorder: const UnderlineInputBorder(
@@ -114,6 +194,14 @@ class _ContactFormsState extends State<ContactForms> {
               const SizedBox( height: 30,),
               ElevatedButton(
                 onPressed: () {
+                  if (nameKey.currentState!.validate() && phoneKey.currentState!.validate()) {
+                    // save to array 
+                    contactList.add({
+                      "avatar": nameController.text[0].toUpperCase(),
+                      "name": nameController.text,
+                      "phone": phoneController.text
+                    });
+                  }
                 },
                 style: ElevatedButton.styleFrom(
                     primary: const Color(0xff6750A4),
@@ -127,66 +215,6 @@ class _ContactFormsState extends State<ContactForms> {
               )
             ],
           )),
-    );
-  }
-}
-
-class allContact extends StatefulWidget {
-  const allContact({super.key});
-
-  @override
-  State<allContact> createState() => _allContactState();
-}
-
-class _allContactState extends State<allContact> {
-  
-  final List<Map<String, String>> contactList = [
-    {"avatar": "L", "name": "Leanne Graham", "phone": "1-770-736-8031 x56442"},
-    {"avatar": "E", "name": "Ervin Howell", "phone": "010-692-6593 x09125"},
-    {"avatar": "C", "name": "Clementine Bauch", "phone": "1-463-123-4447"},
-    {"avatar": "S", "name": "Patricia Lebsack", "phone": "493-170-9623 x156"},
-    {"avatar": "C", "name": "Chelsey Dietrich", "phone": "(254)954-1289"},
-    {"avatar": "K", "name": "Kurtis Weissnat", "phone": "210.067.6132"},
-    {"avatar": "Z", "name": "Zenaya Krakozia", "phone": "434-170-9623 x156"},
-    {"avatar": "D", "name": "Dimitri Rascalov", "phone": "210.067.6132"},
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          backgroundColor: const Color(0xff6750A4),
-          title: const Text('Contacts List', style: TextStyle(color: Colors.white)),
-          centerTitle: true,
-        ),
-      body: ListView.builder(
-        itemCount: contactList.length,
-        itemBuilder: (BuildContext context, int i) {
-          final contact = contactList[i];
-          return ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Color(0xff6750A4),
-              radius: 20,
-              child: Text(
-                contact["avatar"]!,
-                style: const TextStyle(color: Colors.white, fontSize: 20),
-              ),
-            ),
-            title: Text(contact["name"]!),
-            subtitle: Text(contact["phone"]!),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color(0xff6750A4),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const ContactForms()),
-          );
-        },
-        child: const Icon(Icons.add),
-      )
     );
   }
 }
--- 15_Flutter Form (Form Input Button) and Advance Form/contactsapp/lib/main.dart
+++ 15_Flutter Form (Form Input Button) and Advance Form/contactsapp/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: allContact()
+      home: const allContact()
       // home: DateCalendar()
     );
   }
