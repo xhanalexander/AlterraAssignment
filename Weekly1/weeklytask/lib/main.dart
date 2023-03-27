da5897b49107dc98661b0b1c8a621dddfb81f157
xhanalexander <xhanalexander@gmail.com>
2023-03-28 00:10:00 +0700

    major added weekly task

--- Weekly1/weeklytask/lib/main.dart
+++ Weekly1/weeklytask/lib/main.dart
@@ -11,11 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: WelcomePage(),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const WelcomePage(),
+        // home: ContactUs()
     );
   }
 }
@@ -28,50 +29,76 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+  Widget _buildImage(String assetName, [double width = 350]) {
+    return Image.asset('assets/images/$assetName', width: width);
+  }
 
   @override
   Widget build(BuildContext context) {
+    const bodyStyle = TextStyle(color: Colors.black);
+
+    const pageDecoration = PageDecoration(
+      titleTextStyle: TextStyle(
+          color: Color(0xFF0071F2), fontSize: 28, fontWeight: FontWeight.bold),
+      bodyTextStyle: bodyStyle,
+      bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
     return Scaffold(
       body: IntroductionScreen(
         pages: [
           PageViewModel(
-            title: "Selamat Datang di Aplikasi",
-            body: "Aplikasi ini memberikan informasi mengenai kampus dan jurusan yang ada di Indonesia",
-            image: Image.asset(
-              "assets/StudentRead.png",
-              height: 250,
-            ),
-            decoration: const PageDecoration(
-              pageColor: Colors.white,
-              bodyTextStyle: TextStyle(color: Colors.black),
-              titleTextStyle: TextStyle(color: Colors.black, fontSize: 28),
+            title: "Selamat Datang di CampuScholarShip",
+            body:
+                "Aplikasi ini memberikan informasi mengenai kampus dan jurusan yang ada di Indonesia",
+            image: _buildImage('StudentRead.png'),
+            decoration: pageDecoration.copyWith(
+              bodyFlex: 3,
+              imageFlex: 4,
+              safeArea: 80,
             ),
           ),
           PageViewModel(
-            title: "Segera Daftar Sekarang",
-            body: "Untuk mendapatkan informasi mengenai kampus dan jurusan yang ada di Indonesia",
-            image: Image.asset(
-              "assets/Write.png",
-              height: 300,
-            ),
-            decoration: const PageDecoration(
-              pageColor: Colors.white,
-              bodyTextStyle: TextStyle(color: Colors.black),
-              titleTextStyle: TextStyle(color: Colors.black, fontSize: 28),
-            ),
-          ),
+              title: "Segera Daftar Sekarang sebagai Mahasiswa Baru",
+              body:
+                  "Untuk mendapatkan informasi mengenai kampus dan jurusan yang ada di Indonesia",
+              image: _buildImage('Write.png'),
+              decoration: pageDecoration.copyWith(
+                bodyFlex: 3,
+                imageFlex: 5,
+                safeArea: 80,
+              ),
+              footer: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50),
+                child: SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xFF0071F2),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const FormSign()),
+                      );
+                    },
+                    child: const Text(
+                      "Lets Get Started",
+                    ),
+                  ),
+                ),
+              )),
         ],
-        onDone: () {
-          // When done button is press
-        },
-        onSkip: () {
-          // You can also override onSkip callback
-        },
         showSkipButton: false,
-        skip: const Icon(Icons.skip_next),
+        showDoneButton: false,
         next: const Icon(Icons.arrow_forward),
-        done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
-        // globalBackgroundColor: const Color(0xFF0071F2),
+        globalBackgroundColor: Colors.white,
         dotsDecorator: const DotsDecorator(
           size: Size(10.0, 10.0),
           color: Color(0xFFBDBDBD),
@@ -81,7 +108,349 @@ class _WelcomePageState extends State<WelcomePage> {
             borderRadius: BorderRadius.all(Radius.circular(25.0)),
           ),
         ),
-      )
+      ),
+    );
+  }
+}
+
+class FormSign extends StatefulWidget {
+  const FormSign({super.key});
+
+  @override
+  State<FormSign> createState() => _FormSignState();
+}
+
+class _FormSignState extends State<FormSign> {
+  final formKey = GlobalKey<FormState>();
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  final List<Map<String, dynamic>> data = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            padding: const EdgeInsets.all(20),
+            child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "REGISTER CampuScholarShip",
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF0071F2),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: "Name",
+                      prefixIcon: Icon(Icons.person),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your name";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: "Email",
+                      prefixIcon: Icon(Icons.email),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your email";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: passwordController,
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: "Password",
+                      suffixIcon: Icon(Icons.visibility),
+                      prefixIcon: Icon(Icons.lock),
+                    ),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your password";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: "Confirm Password",
+                      suffixIcon: Icon(Icons.visibility),
+                      prefixIcon: Icon(Icons.lock),
+                    ),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please re-enter your confirm password";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 45,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF0071F2),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          setState(() {
+                            data.add({
+                              "name": nameController.text,
+                              "email": emailController.text,
+                              "password": passwordController.text,
+                            });
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: const Text("Success"),
+                                    content: Text(
+                                        "Your account has been created welcome, ${nameController.text}"),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    const WelcomePage()),
+                                          );
+                                        },
+                                        child: const Text("OK"),
+                                      ),
+                                    ],
+                                  );
+                                });
+                          });
+                        }
+                      },
+                      child:
+                          const Text("Sign Up", style: TextStyle(fontSize: 20)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Already have an account?"),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const WelcomePage()),
+                          );
+                        },
+                        child: const Text("Sign In"),
+                      ),
+                    ],
+                  ),
+                  // contact us
+                  SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const ContactUs()),
+                          );
+                        },
+                        child: const Text("Contact Us"),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )));
+  }
+}
+
+class ContactUs extends StatefulWidget {
+  const ContactUs({super.key});
+
+  @override
+  State<ContactUs> createState() => _ContactUsState();
+}
+
+class _ContactUsState extends State<ContactUs> {
+  final formKey = GlobalKey<FormState>();
+  final firstName = TextEditingController();
+  final lastName = TextEditingController();
+  final emailController = TextEditingController();
+  final messagesController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Contact Us"),
+        centerTitle: true,
+        backgroundColor: Color(0xFF0071F2)
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(color: Color.fromARGB(0, 255, 255, 255)),
+                ),
+                Image.asset("assets/images/Contactus.png", width: 400),
+                // const Text("Contact Us", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Color(0xFF0071F2))),
+                const SizedBox(height: 15),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: firstName,
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.person),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(8))),
+                          hintText: 'First Name',
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Please enter your first name";
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: TextFormField(
+                        controller: lastName,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(8))),
+                          hintText: 'Last Name',
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Please enter your last name";
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 15),
+                TextFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                    labelText: "Email",
+                    prefixIcon: Icon(Icons.email),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter your email";
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 15),
+                TextFormField(
+                  controller: messagesController,
+                  maxLines: 5,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                    labelText: "Message",
+                    prefixIcon: Icon(Icons.message),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 50),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 45,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: const Color(0xFF0071F2),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: const Text("Success"),
+                                  content: const Text("Your message has been sent"),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text("OK"),
+                                    ),
+                                  ],
+                                );
+                              });
+                        }
+                      },
+                      child: const Text("Send", style: TextStyle(fontSize: 20)),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(color: Color.fromARGB(0, 255, 255, 255)),
+                ),
+              ],
+          ))
+      ),
     );
   }
 }
