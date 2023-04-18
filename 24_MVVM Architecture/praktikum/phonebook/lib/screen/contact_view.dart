8d78da6a73f26be3bd91fba289927dd9622e2dc9
xhanalexander <xhanalexander@gmail.com>
2023-04-19 04:28:49 +0700

    mvvm updates

--- 24_MVVM Architecture/praktikum/phonebook/lib/screen/contact_view.dart
+++ 24_MVVM Architecture/praktikum/phonebook/lib/screen/contact_view.dart
@@ -30,10 +30,75 @@ class _ContactViewsState extends State<ContactViews> {
     final modelView = Provider.of<ContactViewsModel>(context);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Phonebook'),
+        title: const Text('Contact List'),
         centerTitle: true,
+        backgroundColor: const Color(0xff6750A4),
       ),
-      body: ListView.builder(
+      body: Container(
+        width: double.infinity,
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white38,
+        ),
+        child: ListView.builder(
+          itemCount: modelView.contacts.length,
+          itemBuilder: (context, index) {
+            final contact = modelView.contacts[index];
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundColor: const Color(0xffEADDFF),
+                child: Text(contact.name[0], style: const TextStyle(color: Color(0xff21005D), fontWeight: FontWeight.bold)),
+              ),
+              title: Text(contact.name),
+              subtitle: Text(contact.phone),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      setState(() {});
+                    },
+                    icon: const Icon(Icons.edit_outlined, color: Colors.black),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        showDialog(
+                          context: context, 
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text('Delete Contact'),
+                              content: const Text('Are you sure?'),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {}, 
+                                  child: const Text('Cancel')
+                                ),
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                    setState(() {});
+                                  }, 
+                                  child: const Text('Delete')
+                                ),
+                              ],
+                            );
+                          }
+                        );
+                      });
+                    },
+                    icon: const Icon(Icons.delete_outlined, color: Colors.black),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+
+
+      /* body: ListView.builder(
         itemCount: modelView.contacts.length,
         itemBuilder: (context, index) {
           final contact = modelView.contacts[index];
@@ -42,7 +107,7 @@ class _ContactViewsState extends State<ContactViews> {
             subtitle: Text(contact.phone),
           );
         },
-      ),
+      ), */
     );
   }
 }
